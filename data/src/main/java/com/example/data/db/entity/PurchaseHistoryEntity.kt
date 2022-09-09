7a5599227b098b558340de59650d4c369bf114bb
SB <seungbae2@gmail.com>
2022-09-09 23:08:56 +0900

    PurchaseHistoryConverter add

--- data/src/main/java/com/example/data/db/entity/PurchaseHistoryEntity.kt
+++ data/src/main/java/com/example/data/db/entity/PurchaseHistoryEntity.kt
@@ -2,11 +2,15 @@ package com.example.data.db.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.example.data.db.converter.PurchaseHistoryConverter
 import com.example.domain.model.BasketProduct
 import com.example.domain.model.PurchaseHistory
 import java.time.ZonedDateTime
 
 @Entity(tableName = "history")
+@TypeConverters(PurchaseHistoryConverter::class)
 data class PurchaseHistoryEntity(
     val basketList: List<BasketProduct>,
     val purchaseAt: ZonedDateTime
