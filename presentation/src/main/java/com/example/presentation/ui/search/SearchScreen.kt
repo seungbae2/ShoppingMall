2d72f8631a0acd063bd2c98eb520fd10b97531ae
SB <seungbae2@gmail.com>
2023-07-10 23:10:22 +0900

    SearchScreen 수정

--- presentation/src/main/java/com/example/presentation/ui/search/SearchScreen.kt
+++ presentation/src/main/java/com/example/presentation/ui/search/SearchScreen.kt
@@ -5,24 +5,31 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.BottomSheetValue
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,13 +41,87 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.domain.model.SearchFilter
 import com.example.presentation.ui.component.ProductCard
+import com.example.presentation.ui.theme.Purple200
+import com.example.presentation.ui.theme.Purple500
 import com.example.presentation.viewmodel.search.SearchViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SearchScreen(
     navHostController: NavHostController,
     viewModel: SearchViewModel = hiltViewModel()
+) {
+    val searchFilter by viewModel.searchFilters.collectAsState()
+    val sheetState = rememberBottomSheetState(initialValue = BottomSheetValue.Collapsed)
+    val scaffoldState = rememberBottomSheetScaffoldState(bottomSheetState = sheetState)
+    val scope = rememberCoroutineScope()
+    var currentFilterType by remember { mutableStateOf<SearchFilter.Type?>(null) }
+
+    BottomSheetScaffold(
+        scaffoldState= scaffoldState,
+        sheetShape = RoundedCornerShape(10.dp, 10.dp, 0.dp, 0.dp),
+        sheetContent = {
+
+        },
+        sheetPeekHeight = 0.dp
+    ) {
+        SearchContent(viewModel, navHostController) {
+            scope.launch {
+                currentFilterType = it
+                sheetState.expand()
+            }
+        }
+    }
+}
+
+@Composable
+fun SearchFilterCategoryContent(filter: SearchFilter.CategoryFilter, onCompleteFilter:(SearchFilter) -> Unit) {
+    //헤더
+    //카테고리 리스트
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+    ) {
+        Text(
+            text = "카테고리 필터",
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(10.dp)
+        )
+        LazyColumn(modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(10.dp)
+        ) {
+            items(filter.categories.size) { index ->
+                val category = filter.categories[index]
+                Button(
+                    onClick = {
+                        filter.selectedCategory = category
+                        onCompleteFilter(filter)
+                    },
+                    colors = ButtonDefaults.buttonColors(backgroundColor = if(filter.selectedCategory == category) Purple500
+                    else Purple200))
+                {
+                    Text(
+                        fontSize = 18.sp,
+                        text = category.categoryName
+                    )
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun SearchContent(
+    viewModel: SearchViewModel,
+    navHostController: NavHostController,
+    openFilterDialog: (SearchFilter.Type) -> Unit
 ) {
     val searchResult by viewModel.searchResult.collectAsState()
     val searchKeywords by viewModel.searchKeywords.collectAsState(listOf())
