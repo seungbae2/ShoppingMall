a3e3baccc180634f6d03c0e8139431bb3e0eb3f6
SB <seungbae2@gmail.com>
2023-07-13 22:46:18 +0900

    AccountInfo add PreferenceDataSource add setup for google sign in

---
+++ domain/src/main/java/com/example/domain/model/AccountInfo.kt
@@ -0,0 +1,7 @@
+package com.example.domain.model
+
+data class AccountInfo(val tokenId: String, val name: String, val type: Type, val profileImageUrl: String) {
+    enum class Type {
+        GOOGLE
+    }
+}
