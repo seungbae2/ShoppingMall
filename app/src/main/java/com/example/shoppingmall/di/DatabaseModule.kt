be72b3943cb61be4350a0a5cdce4d56be7332ba7
SB <seungbae2@gmail.com>
2023-08-02 23:10:06 +0900

    BasketDao 수정

--- app/src/main/java/com/example/shoppingmall/di/DatabaseModule.kt
+++ app/src/main/java/com/example/shoppingmall/di/DatabaseModule.kt
@@ -3,6 +3,7 @@ package com.example.shoppingmall.di
 import android.content.Context
 import androidx.room.Room
 import com.example.data.db.ApplicationDatabase
+import com.example.data.db.dao.BasketDao
 import com.example.data.db.dao.LikeDao
 import com.example.data.db.dao.SearchDao
 import dagger.Module
@@ -39,5 +40,11 @@ object DatabaseModule {
     @Singleton
     fun provideLikeDao(database: ApplicationDatabase): LikeDao {
         return database.likeDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBasketDao(database: ApplicationDatabase): BasketDao {
+        return database.basketDao()
     }
 }
--- data/src/main/java/com/example/data/db/ApplicationDatabase.kt
+++ data/src/main/java/com/example/data/db/ApplicationDatabase.kt
@@ -18,7 +18,7 @@ import com.example.data.db.entity.SearchKeywordEntity
         BasketProductEntity::class,
         SearchKeywordEntity::class
     ],
-    version = 1
+    version = 2
 )
 abstract class ApplicationDatabase : RoomDatabase() {
     companion object {
