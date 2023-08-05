c6797335050e98b92fd685256d71c8cdea23aad4
SB <seungbae2@gmail.com>
2023-08-05 21:56:01 +0900

    addBasket function add

--- data/src/main/java/com/example/data/repository/ProductDetailRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/ProductDetailRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.example.data.repository
 
 import com.example.data.datasource.ProductDataSource
+import com.example.data.db.dao.BasketDao
+import com.example.data.db.entity.toBasketProductEntity
 import com.example.domain.model.Product
 import com.example.domain.repository.ProductDetailRepository
 import kotlinx.coroutines.flow.Flow
@@ -8,11 +10,21 @@ import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class ProductDetailRepositoryImpl @Inject constructor(
-    private val dataSource: ProductDataSource
+    private val dataSource: ProductDataSource,
+    private val basketDao: BasketDao,
 ): ProductDetailRepository {
     override fun getProductDetail(productId: String): Flow<Product> {
         return dataSource.getHomeComponents().map { products ->
             products.filterIsInstance<Product>().first { it.productId == productId }
+        }
+    }
+
+    override suspend fun addBasket(product: Product) {
+        val alreadyExistProduct = basketDao.get(product.productId)
+        if (alreadyExistProduct == null) {
+            basketDao.insert(product.toBasketProductEntity())
+        } else {
+            basketDao.insert(alreadyExistProduct.copy(count = alreadyExistProduct.count + 1))
         }
     }
 }
