52f1b4655287b827cf89f8c6b55b57ff750b2c50
SB <seungbae2@gmail.com>
2023-08-12 22:56:33 +0900

    BasketScreen file add

---
+++ presentation/src/main/java/com/example/presentation/ui/basket/BasketScreen.kt
@@ -0,0 +1,14 @@
+package com.example.presentation.ui.basket
+
+import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.presentation.viewmodel.basket.BasketViewModel
+
+@Composable
+fun BasketScreen(
+    scaffoldState: ScaffoldState,
+    viewModel: BasketViewModel = hiltViewModel()
+) {
+
+}
