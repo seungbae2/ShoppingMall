ae50976fbe80b181abf97dc1fd789540e0939a20
SB <seungbae2@gmail.com>
2023-08-15 23:02:14 +0900

    BasketProductCard Composable function add

--- presentation/src/main/java/com/example/presentation/ui/basket/BasketScreen.kt
+++ presentation/src/main/java/com/example/presentation/ui/basket/BasketScreen.kt
@@ -1,8 +1,33 @@
 package com.example.presentation.ui.basket
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.domain.model.BasketProduct
+import com.example.domain.model.Product
+import com.example.presentation.R
+import com.example.presentation.ui.component.Price
 import com.example.presentation.viewmodel.basket.BasketViewModel
 
 @Composable
@@ -11,4 +36,55 @@ fun BasketScreen(
     viewModel: BasketViewModel = hiltViewModel()
 ) {
 
+}
+
+@Composable
+fun BasketProductCard(basketProduct: BasketProduct, removeProduct: (Product) -> Unit) {
+    Box(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.product_image),
+                "description",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .width(120.dp)
+                    .aspectRatio(1f)
+            )
+            Column(
+                modifier = Modifier
+                    .padding(10.dp, 0.dp, 0.dp, 0.dp)
+                    .weight(1f)
+            ) {
+                Text(
+                    fontSize = 14.sp,
+                    text = basketProduct.product.shop.shopName,
+                    modifier = Modifier.padding(0.dp, 10.dp, 0.dp, 0.dp)
+                )
+
+                Text(
+                    fontSize = 14.sp,
+                    text = basketProduct.product.productName,
+                    modifier = Modifier.padding(0.dp, 10.dp, 0.dp, 0.dp)
+                )
+                Price(product = basketProduct.product)
+            }
+            Text(
+                text = "${basketProduct.count} 개",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(30.dp)
+            )
+        }
+        IconButton(
+            onClick = { removeProduct(basketProduct.product) },
+            modifier = Modifier.align(Alignment.TopEnd)
+        ) {
+            Icon(Icons.Filled.Close, "CloseIcon")
+        }
+    }
 }
