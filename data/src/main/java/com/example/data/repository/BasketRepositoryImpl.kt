9289776c8c38a3a4ced9fd70182a2e5b5a4b6227
SB <seungbae2@gmail.com>
2023-09-14 23:46:32 +0900

    BasketRepositoryImpl checkoutBasket add

--- data/src/main/java/com/example/data/repository/BasketRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/BasketRepositoryImpl.kt
@@ -1,16 +1,22 @@
 package com.example.data.repository
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.data.db.dao.BasketDao
+import com.example.data.db.dao.PurchaseHistoryDao
+import com.example.data.db.entity.PurchaseHistoryEntity
 import com.example.data.db.entity.toDomainModel
 import com.example.domain.model.BasketProduct
 import com.example.domain.model.Product
 import com.example.domain.repository.BasketRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import java.time.ZonedDateTime
 import javax.inject.Inject
 
 class BasketRepositoryImpl @Inject constructor(
-    private val basketDao: BasketDao
+    private val basketDao: BasketDao,
+    private val purchaseHistoryDao: PurchaseHistoryDao,
 ) : BasketRepository {
     override fun getBasketProducts(): Flow<List<BasketProduct>> {
         return basketDao.getAll().map { list ->
@@ -20,5 +26,11 @@ class BasketRepositoryImpl @Inject constructor(
 
     override suspend fun removeBasketProduct(product: Product) {
         return basketDao.delete(product.productId)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override suspend fun checkoutBasket(products: List<BasketProduct>) {
+        purchaseHistoryDao.insert(PurchaseHistoryEntity(products, ZonedDateTime.now()))
+        basketDao.deleteAll()
     }
 }
